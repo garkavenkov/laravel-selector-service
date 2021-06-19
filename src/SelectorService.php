f6957015d87f4a67a06ea28bbaff003a9657e48a
Maksim Garkavenkov <maksim.garkavenkov@gmail.com>
2021-06-19 13:27:47 +0300

    Added functionality for relationship

--- src/SelectorService.php
+++ src/SelectorService.php
@@ -1,8 +1,28 @@
 <?php
 
-namespace Garkavenkov\Laravel\SelectorService;
+namespace Garkavenkov\Laravel;
 
-class Selector 
+class SelectorService
 {
-   
+    public function get($model, $parameters = [])
+    {
+        $parameters = request()->input();
+
+        $relationship = $this->getRelationship($parameters);
+
+    }
+
+
+    private function getRelationship(&$parameters)
+    {
+        $relationship = [];
+
+        if (array_key_exists('with', $parameters)) {
+            $relationship = explode(',', $parameters['with']);
+            unset($parameters['with']);
+        }
+
+        return $relationship;
+    }
+
 }
